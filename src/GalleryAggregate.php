6a2e80bc2572455b4a9271c1796f68232afb2830
Eric Jacobson <contact@efjacobson.com>
2020-10-13 17:17:43 -0700

    sy5 - Aggregates and other wip

--- src/GalleryAggregate.php
+++ src/GalleryAggregate.php
@@ -1,1 +1,26 @@
-todo
+<?php
+declare(strict_types=1);
+
+namespace Triniti\Curator;
+
+use Gdbots\Ncr\Aggregate;
+
+class GalleryAggregate extends Aggregate
+{
+    /**
+     * This is for legacy uses for command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param string $name
+     * @param array $arguments
+     *
+     * @return mixed
+     */
+    public function __call(string $name, array $arguments)
+    {
+        $newName = str_replace('Gallery', 'Node', $name);
+        if ($newName !== $name && is_callable([$this, $newName])) {
+            return $this->$newName(...$arguments);
+        }
+    }
+}
--- src/TeaserAggregate.php
+++ src/TeaserAggregate.php
@@ -1,1 +1,26 @@
-todo
+<?php
+declare(strict_types=1);
+
+namespace Triniti\Curator;
+
+use Gdbots\Ncr\Aggregate;
+
+class TeaserAggregate extends Aggregate
+{
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param string $name
+     * @param array  $arguments
+     *
+     * @return mixed
+     */
+    public function __call(string $name, array $arguments)
+    {
+        $newName = str_replace('Teaser', 'Node', $name);
+        if ($newName !== $name && is_callable([$this, $newName])) {
+            return $this->$newName(...$arguments);
+        }
+    }
+}
